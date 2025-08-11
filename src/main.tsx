e822ae049c01fee32a1ee31e11281f2784256d96
nhinq2 <47347404+nhinq2@users.noreply.github.com>
2025-08-11 21:21:27 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,5 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    
-  <script async  
-  src="https://chat.taggoai.com/v2.js"
-  data-taggo-botid="658845b5edbcbf2f6822e4b3"></script>
-  
   </StrictMode>
 );
